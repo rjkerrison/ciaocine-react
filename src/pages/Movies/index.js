840fb58e0da285a1e5c1650a391c62a30fe41c4f
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-15 13:27:15 +0200

    movies index page JSX

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,27 +1,33 @@
-// <section class='movies-section'>
-//   <h2>
-//     {{weekdaydate chosenDate}}
-//   </h2>
-//    <p>{{movies.length}} films are showing on {{weekdaydate chosenDate}} matching your filters</p>
-// <nav>
-//   {{> moviesFilters label='sur le' urls=calendarUrls}}
-//   {{> moviesFilters label='à partir de' urls=hoursUrls}}
-//   {{> moviesFilters label='+ de filtres' urls=filtersUrls}}
-// </nav>
-// <div class='movies'>
-//   {{#each movies}}
-//     <div class='movie overlay-container expander-container'>
-//       {{> movieHeading movie=movie}}
-//       {{> movieShowtimes showtimes=showtimes}}
-//     </div>
-//   {{/each}}
-// </div>
-// </section>
-
 import React from 'react'
+import MovieHeading from '../../components/MovieHeading'
 
-const Movies = () => {
-  return <div>Movies</div>
+const weekdaydate = (d) => d?.toString()
+
+const Movies = ({ chosenDate }) => {
+  const movies = []
+
+  return (
+    <section className='movies-section'>
+      <h2>{weekdaydate(chosenDate)}</h2>
+      <p>
+        {movies.length} films are showing on {weekdaydate(chosenDate)} matching
+        your filters
+      </p>
+      <nav>
+        {/* {{> moviesFilters label='sur le' urls=calendarUrls}}
+  {{> moviesFilters label='à partir de' urls=hoursUrls}}
+  {{> moviesFilters label='+ de filtres' urls=filtersUrls}} */}
+      </nav>
+      <div className='movies'>
+        {movies.map((movie) => (
+          <div className='movie overlay-container expander-container'>
+            <MovieHeading movie={movie} />
+            {/* <MovieShowtimes showtimes={showtimes} /> */}
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Movies
