5f44730fc1644a701a44362d19ac3ca701435706
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-15 13:29:02 +0200

    renaming for easier file finding

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import MovieHeading from '../../components/MovieHeading'
+import MovieHeading from '../components/MovieHeading'
 
 const weekdaydate = (d) => d?.toString()
 
