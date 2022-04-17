f3c6a18f5b0c19a6d047a411ddc58b4b562da92f
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-17 03:22:59 +0200

    MoviePopup styling and data display

--- src/components/movies/MoviePopup.jsx
+++ src/components/movies/MoviePopup.jsx
@@ -1,22 +1,24 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { API_URL } from '../../utils/consts'
+import { formatAs } from '../../utils/formatDate'
 
 const getData = async (movieId) => {
   const {
-    data: { movie },
+    data: { movie, tmdbInfo },
   } = await axios({
     baseURL: API_URL,
     url: `/movies/${movieId}/`,
   })
-  return movie
+  return { ...movie, extra: tmdbInfo[0] }
 }
 
 const MoviePopup = ({ movieId, active, close }) => {
   const [movieInfo, setMovieInfo] = useState(null)
+  const [classes, setClasses] = useState(['movie-popup'])
 
   useEffect(() => {
-    if (!movieId || !active) {
+    if (!movieId) {
       return
     }
     const getMovie = async () => {
@@ -26,11 +28,46 @@ const MoviePopup = ({ movieId, active, close }) => {
     getMovie()
   }, [movieId])
 
+  useEffect(() => {
+    if (active) {
+      const timeout = setTimeout(() => setClasses((c) => [...c, 'active']), 500)
+      return () => clearTimeout(timeout)
+    }
+  }, [active])
+
+  const collapseThenHide = () => {
+    setClasses((c) => c.filter((a) => a !== 'active'))
+    const timeout = setTimeout(close, 500)
+    return () => clearTimeout(timeout)
+  }
+
   if (!active || !movieInfo) {
     return <></>
   }
 
-  return <p onClick={close}>{JSON.stringify(movieInfo, null, 2)}</p>
+  return (
+    <article className={classes.join(' ')} onClick={collapseThenHide}>
+      <div
+        onClick={(e) => e.stopPropagation()}
+        style={{
+          '--backdrop-path': `url(https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${movieInfo.extra.backdrop_path})`,
+        }}
+      >
+        <h2>{movieInfo.extra.original_title}</h2>
+        <p>{movieInfo.extra.overview}</p>
+        <p>{movieInfo.extra.vote_average} / 10 on TMDB</p>
+        <p>Released {formatAs.date(movieInfo.extra.release_date)}</p>
+        <ul>
+          {movieInfo.extra.crew.slice(0, 5).map(({ job, name, id }) => (
+            <li key={`${job} ${name}`}>
+              {job}: {name}
+            </li>
+          ))}
+        </ul>
+        <button onClick={collapseThenHide}>Close</button>
+      </div>
+    </article>
+  )
 }
 
 export default MoviePopup
