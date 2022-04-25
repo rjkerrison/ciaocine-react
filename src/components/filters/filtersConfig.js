5a08a74f44e41ac9184316c22badf75baf881b94
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-25 20:33:11 +0200

    hour filters to be sent in UTC

--- src/components/filters/filtersConfig.js
+++ src/components/filters/filtersConfig.js
@@ -14,11 +14,13 @@ const daysAheadOptions = () => {
   })
 }
 
+const date = new Date()
+const offset = Math.floor(date.getTimezoneOffset() / 60)
 const getHoursOptions = () => {
   return [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map(
     (hour) => {
       return {
-        value: hour,
+        value: hour + offset,
         label: hour + 'h',
       }
     }
