29d680547d48ae19d3037faf7dff4aace4d13c63
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-06-14 17:10:32 +0200

    movie headings with directors

--- src/components/MovieHeading.jsx
+++ src/components/MovieHeading.jsx
@@ -1,11 +1,14 @@
 import { stripProtocol } from '../utils/urls'
 import PopupLink from './shared/PopupLink'
 
-const MovieHeading = ({ title, poster, slug }) => {
+import './MovieHeading.scss'
+
+const MovieHeading = ({ title, poster, slug, castingShort: { directors } }) => {
   return (
     <>
       <PopupLink to={`/movies/${slug}`} className='movie-heading'>
         <h3>{title}</h3>
+        <p>de {directors}</p>
       </PopupLink>
       <picture className='poster' title={title}>
         <PopupLink to={`/movies/${slug}`}>
