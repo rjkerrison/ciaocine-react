59b4d7315e6b3e3630b3711e160705782db6723b
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-06-14 15:16:51 +0200

    movie slugs in popup links

--- src/components/MovieHeading.jsx
+++ src/components/MovieHeading.jsx
@@ -1,14 +1,14 @@
 import { stripProtocol } from '../utils/urls'
 import PopupLink from './shared/PopupLink'
 
-const MovieHeading = ({ title, poster, _id }) => {
+const MovieHeading = ({ title, poster, slug }) => {
   return (
     <>
-      <PopupLink to={`/movies/${_id}`} className='movie-heading'>
+      <PopupLink to={`/movies/${slug}`} className='movie-heading'>
         <h3>{title}</h3>
       </PopupLink>
       <picture className='poster' title={title}>
-        <PopupLink to={`/movies/${_id}`}>
+        <PopupLink to={`/movies/${slug}`}>
           <img src={stripProtocol(poster)} alt={title} />
         </PopupLink>
       </picture>
