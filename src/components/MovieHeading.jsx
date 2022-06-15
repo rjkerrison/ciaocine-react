49e424879a658b95c1d1feaf3cec3cf8ce439508
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-06-15 16:19:08 +0200

    castingShort null fix

--- src/components/MovieHeading.jsx
+++ src/components/MovieHeading.jsx
@@ -3,12 +3,12 @@ import PopupLink from './shared/PopupLink'
 
 import './MovieHeading.scss'
 
-const MovieHeading = ({ title, poster, slug, castingShort: { directors } }) => {
+const MovieHeading = ({ title, poster, slug, castingShort }) => {
   return (
     <>
       <PopupLink to={`/movies/${slug}`} className='movie-heading'>
         <h3>{title}</h3>
-        <p>de {directors}</p>
+        {castingShort && <p>de {castingShort?.directors}</p>}
       </PopupLink>
       <picture className='poster' title={title}>
         <PopupLink to={`/movies/${slug}`}>
