291a0157f5f56afda992dfc512f680028a57524a
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-06-19 11:44:24 +0200

    removing unnecessary escape characters from slug conversion regexes

--- src/components/MovieHeading.jsx
+++ src/components/MovieHeading.jsx
@@ -9,9 +9,9 @@ const convertToSlug = (text) => {
     .toString() // Cast to string
     .normalize('NFKD') // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
     .toLowerCase() // Convert the string to lowercase letters
-    .replace(/[^\w\-\s]+/g, '') // Remove all non-word chars
+    .replace(/[^-\w\s]+/g, '') // Remove all non-word chars
     .trim() // Remove whitespace from both sides of a string
-    .replace(/[\-\s]+/g, '-') // Replace spaces with -
+    .replace(/[-\s]+/g, '-') // Replace spaces with -
 }
 
 const MovieHeading = ({
