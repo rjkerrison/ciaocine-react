b40adc7e22012f72aafa38afdf4a2ae0970c5e67
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-09-22 13:07:08 +0200

    using colour to delimit text

--- src/components/MovieHeading.jsx
+++ src/components/MovieHeading.jsx
@@ -3,32 +3,47 @@ import { stripProtocol } from '../utils/urls'
 import { formatAs } from '../utils/formatDate'
 
 import './MovieHeading.scss'
+import { useMemo } from 'react'
 
 const MovieHeading = ({
   title,
   poster,
+  images,
   slug,
   originalTitle,
   castingShort,
   releaseDate,
+  externalIdentifiers,
 }) => {
+  const posterToShow = useMemo(
+    () => stripProtocol(images?.poster ?? poster),
+    [images?.poster, poster]
+  )
+
+  const secondTitle = externalIdentifiers?.tmdb?.title || originalTitle
+
   return (
     <>
       <div className='movie-heading'>
         <PopupLink to={`/movies/${slug}`}>
           <h3>
-            {title} ({formatAs.yearMonthDate(releaseDate).year})
-            {originalTitle && originalTitle !== title && (
-              <span> / {originalTitle}</span>
+            <span>{title}</span>
+            {secondTitle && secondTitle !== title && (
+              <span className='alternative-title'>{secondTitle}</span>
             )}
           </h3>
-          {castingShort && <p>de {castingShort?.directors}</p>}
+          <p>
+            {castingShort && <>de {castingShort?.directors} </>}
+            <span className='year'>
+              {formatAs.yearMonthDate(releaseDate).year}
+            </span>
+          </p>
         </PopupLink>
       </div>
 
       <picture className='poster' title={title}>
         <PopupLink to={`/movies/${slug}`}>
-          <img src={stripProtocol(poster)} alt={title} />
+          <img src={posterToShow} alt={title} />
         </PopupLink>
       </picture>
     </>
